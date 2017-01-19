3c163148d378fb4cff4e3c2622ef91b0e3293d3b
Jarrod Valentine <jkvalentine7695@gmail.com>
2017-01-19 11:45:29 -0800

    update fraudly app

--- fraudly_app/fraudly_app.py
+++ fraudly_app/fraudly_app.py
@@ -1,12 +1,56 @@
-from flask import Flask
-from flask import request
-app = Flask(__name__)
+from flask import Flask, request, render_template
+import json
+import requests
+import socket
+import time
+from datetime import datetime
 
-# our home page
-#============================================
-@app.route('/')
+#Initialize pickle app
+app = Flask(__name__)
+PORT = 8080
+
+with open
+
+@app.route('/index')
 def index():
-    return '<h1> Something </h1>'
+	return render_template('index.html')
+
+@app.route('/score', methods=['POST'])
+
+@app.route
+#def score():
+#     DATA.append(json.dumps(request.json, sort_keys=True, indent=4, separators=(',', ': ')))
+#     TIMESTAMP.append(time.time())
+#     return ""
+
+
+# @app.route('/check')
+# def check():
+#     line1 = "Number of data points: {0}".format(len(DATA))
+#     if DATA and TIMESTAMP:
+#         dt = datetime.fromtimestamp(TIMESTAMP[-1])
+#         data_time = dt.strftime('%Y-%m-%d %H:%M:%S')
+#         line2 = "Latest datapoint received at: {0}".format(data_time)
+#         line3 = DATA[-1]
+#         output = "{0}\n\n{1}\n\n{2}".format(line1, line2, line3)
+#     else:
+#         output = line1
+#     return output, 200, {'Content-Type': 'text/css; charset=utf-8'}
+
+
+# def register_for_ping(ip, port):
+#     registration_data = {'ip': ip, 'port': port}
+#     requests.post(REGISTER_URL, data=registration_data)
+
+@app.route('/about_us')
+def about_us():
+	return render_template('about_us.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6969, debug=True)
+    # Register for pinging service
+    #ip_address = socket.gethostbyname(socket.gethostname())
+    #print "attempting to register %s:%d" % (ip_address, PORT)
+    #register_for_ping(ip_address, str(PORT))
+
+    # Start Flask app
+    app.run(host='0.0.0.0', port=PORT, debug=True)
